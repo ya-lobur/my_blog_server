c4f8e7ab3caac9d6ed76b66a756be72177b73b0c
Лобурь Ярослав <lobur@rasse.ru>
2020-12-13 11:35:25 +0300

    * добавляет модель Blog * добавляет миграцию

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    """
+    Блог
+    """
+    owner = models.OneToOneField(verbose_name='Blog owner', to='auth.User', on_delete=models.CASCADE, related_name='blog')
+    description = models.TextField(blank=True)
+    created = models.DateTimeField('Date of creation', auto_now_add=True)
+    updated = models.DateTimeField('Date of last update', auto_now=True, blank=True)
+
+    def __str__(self):
+        return f"Blog belongs to {self.owner.get_full_name()} (id: {self.owner_id})"
