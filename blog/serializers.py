f9d8da1150a2e39cc84963908f04b53e04e5ac1a
Лобурь Ярослав <lobur@rasse.ru>
2020-12-15 21:15:25 +0300

    * добавляет сериализатор поста * обновляет сериализатор блога

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from blog.models import Blog
+from blog.models import Blog, Post
+
+
+class PostModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        exclude = ('created', 'updated')
 
 
 class BlogModelSerializer(serializers.ModelSerializer):
+    posts = PostModelSerializer(many=True, read_only=True)
+
     class Meta:
         model = Blog
-        fields = ('id', 'description')
+        fields = ('id', 'description', 'posts')
