0d4da21562c8eb120b7c4a056f422740066d0084
Лобурь Ярослав <lobur@rasse.ru>
2021-01-16 14:03:02 +0300

    * меняет репрезентацию поля author в PostModelSerializer

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from blog.models import Blog, Post
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'first_name', 'last_name')
 
 
 class PostModelSerializer(serializers.ModelSerializer):
@@ -8,20 +15,21 @@ class PostModelSerializer(serializers.ModelSerializer):
     Сериализатор для операций чтения и обновления
     """
     blog_id = serializers.PrimaryKeyRelatedField(read_only=True)
-    author_id = serializers.PrimaryKeyRelatedField(read_only=True)
     liked_by = serializers.ListField(read_only=True)
+    author = AuthorSerializer(read_only=True)
+
+    # def get_author(self, obj):
+    #     return AuthorSerializer(obj.author).data
 
     class Meta:
         model = Post
-        exclude = ('created', 'updated', 'blog', 'author')
+        exclude = ('created', 'updated', 'blog')
 
 
 class BlogModelSerializer(serializers.ModelSerializer):
     """
     Сериализатор для операций чтения/обновления/создания
     """
-
-    # posts = PostModelSerializer(many=True, read_only=True)
 
     class Meta:
         model = Blog
