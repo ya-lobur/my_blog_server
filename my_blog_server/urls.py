e980d6bf6f93629ebba29f8c2cb3c73797768b5c
Лобурь Ярослав <lobur@rasse.ru>
2021-01-30 14:48:42 +0300

    * переопределяет дефолтную пагинацию

--- my_blog_server/urls.py
+++ my_blog_server/urls.py
@@ -6,7 +6,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-    openapi.Info(title="Blog Server API", default_version='v1', description="Test description", ),
+    openapi.Info(title="Blog Server API", default_version='v1', ),
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
