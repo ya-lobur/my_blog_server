2585bc8cef3740acc96d750b2c241a7b25d7e567
Лобурь Ярослав <lobur@rasse.ru>
2021-02-22 15:49:12 +0300

    * добавляет возможность прикреплять картинку к посту

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,19 @@
+from datetime import date
+
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 from my_blog_server import settings
 
 
+def get_post_media_path(instance, filename):
+    today = date.today()
+    return f'posts/profile/{instance.author.pk}/{today.year}/{today.month}/{today.day}/{filename}'
+
+
 class Blog(models.Model):
     owner = models.OneToOneField(verbose_name='Blog owner', to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog')
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, null=True)
     created = models.DateTimeField('Date of creation', auto_now_add=True)
     updated = models.DateTimeField('Date of last update', auto_now=True, blank=True)
 
@@ -19,6 +26,7 @@ class Post(models.Model):
     title = models.TextField(blank=True)
     description = models.TextField(blank=True)
     text_content = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to=get_post_media_path, blank=True, null=True)
     author = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
     liked_by = ArrayField(models.PositiveIntegerField(), default=list)
     created = models.DateTimeField('Date of creation', auto_now_add=True)
@@ -26,6 +34,15 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-created']
+
+    def save(self, *args, **kwargs):
+        try:
+            stored_post = self.__class__.objects.get(pk=self.pk)
+            if stored_post.image != self.image:
+                stored_post.image.delete(save=False)
+        except self.DoesNotExist:
+            pass
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Post: {self.pk} from blog {self.blog.pk} by {self.author_id}"
